ad06eab9e3c5a9d5d68e84058bd73592cb49c46b
Roger Ignazio <roger@puppetlabs.com>
2014-11-06 14:07:04 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jenkins-standalone
+Everything needed to run a Jenkins master on Apache Mesos and Marathon.
+
+## Usage
+```
+git clone https://gist.github.com/61ee35a61acfb4aefcc5.git jenkins-standalone && \
+  cd jenkins-standalone; \
+  ./jenkins-standalone.sh
+```
---
+++ jenkins-standalone.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+JENKINS_WAR_MIRROR="http://mirrors.jenkins-ci.org/war"
+JENKINS_VERSION="latest"
+JENKINS_PLUGINS_MIRROR="http://updates.jenkins-ci.org"
+JENKINS_PLUGINS_BASEURL="${JENKINS_PLUGINS_MIRROR}/latest"
+
+command -v wget > /dev/null
+
+# Jenkins WAR file
+if [[ ! -f "jenkins.war" ]]; then
+    wget -q "${JENKINS_WAR_MIRROR}/${JENKINS_VERSION}/jenkins.war"
+fi
+
+# Jenkins plugins
+JENKINS_PLUGINS=(git git-client greenballs hipchat mesos metadata parameterized-trigger saferestart token-macro)
+
+[[ ! -d "plugins" ]] && mkdir "plugins"
+for plugin in ${JENKINS_PLUGINS[@]}; do
+    wget -q -P plugins "${JENKINS_PLUGINS_BASEURL}/${plugin}.hpi"
+done
+
+# Start the Jenkins master
+java -jar jenkins.war \
+    -Djava.awt.headless=true \
+    --webroot=war \
+    --httpPort=${PORT}0 \
+    --ajp13Port=-1 \
+    --httpListenAddress=0.0.0.0 \
+    --ajp13ListenAddress=127.0.0.1 \
+    --preferredClassLoader=java.net.URLClassLoader \
+    --logfile=../jenkins.log
+
