29c558c19280978404c2c47b0adb494cbf8ca37f
Roger Ignazio <roger@puppetlabs.com>
2014-11-14 20:24:07 -0800

    Use latest LTS release, install Logstash plugin

--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+JENKINS_WAR_MIRROR="http://mirrors.jenkins-ci.org/war-stable"
+JENKINS_VERSION="1.580.1"
+JENKINS_PLUGINS_MIRROR="http://updates.jenkins-ci.org"
+JENKINS_PLUGINS_BASEURL="${JENKINS_PLUGINS_MIRROR}/latest"
+JENKINS_PLUGINS=(credentials email-ext git git-client greenballs hipchat \
+    logstash mesos metadata parameterized-trigger saferestart scm-api \
+    ssh-credentials token-macro)
+
+# Ensure we have an accessible wget
+command -v wget > /dev/null
+if [[ $? != 0 ]]; then
+    echo "Error: wget not found in \$PATH"
+    echo
+    exit 1
+fi
+
 # Accept ZooKeeper paths on the command line
 if [[ ! $# > 1 ]]; then
     echo "Usage: $0 -z zk://10.132.188.212:2181[, ... ]/mesos"
@@ -23,23 +39,12 @@ while [[ $# > 1 ]]; do
     esac
 done
 
-JENKINS_WAR_MIRROR="http://mirrors.jenkins-ci.org/war"
-JENKINS_VERSION="latest"
-JENKINS_PLUGINS_MIRROR="http://updates.jenkins-ci.org"
-JENKINS_PLUGINS_BASEURL="${JENKINS_PLUGINS_MIRROR}/latest"
-
-command -v wget > /dev/null
-
 # Jenkins WAR file
 if [[ ! -f "jenkins.war" ]]; then
     wget -q "${JENKINS_WAR_MIRROR}/${JENKINS_VERSION}/jenkins.war"
 fi
 
 # Jenkins plugins
-JENKINS_PLUGINS=(credentials email-ext git git-client greenballs hipchat mesos \
-    metadata parameterized-trigger saferestart scm-api ssh-credentials \
-    token-macro)
-
 [[ ! -d "plugins" ]] && mkdir "plugins"
 for plugin in ${JENKINS_PLUGINS[@]}; do
     wget -q -P plugins "${JENKINS_PLUGINS_BASEURL}/${plugin}.hpi"
