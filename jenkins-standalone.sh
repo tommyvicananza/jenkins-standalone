dd6e03685d1ac7493e0d6cef4c3920f6b359de2a
Roger Ignazio <roger@puppetlabs.com>
2014-12-16 11:04:21 -0800

    bump jenkins to 1.580.2

--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -2,7 +2,7 @@
 set -e
 
 # $JENKINS_VERSION should be an LTS release
-JENKINS_VERSION="1.580.1"
+JENKINS_VERSION="1.580.2"
 
 # List of Jenkins plugins, in the format "${PLUGIN_NAME}/${PLUGIN_VERSION}"
 JENKINS_PLUGINS=(\
