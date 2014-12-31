d2e1e0ddee76772b40a44514cde183e2b84207b1
Roger Ignazio <roger@puppetlabs.com>
2014-12-30 17:06:26 -0800

    install the jenkins job-dsl plugin

--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -12,6 +12,7 @@ JENKINS_PLUGINS=(\
     "git-client/1.12.0"          \
     "greenballs/1.14"            \
     "hipchat/0.1.8"              \
+    "job-dsl/1.27"               \
     "logstash/1.0.3"             \
     "metadata/1.1.0b"            \
     "mesos/0.5.0"                \
